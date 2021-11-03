dff7e20f95ef1614e4223e6e79e5da4378b4515c
Nathan Batchelder <natebatchelder@gmail.com>
2021-11-03 10:22:34 -0700

    working on api call for map latlng

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,110 @@
-let map;
+// variables for API keys and ID
+var appID = "";
+var appKey = "";
+
+// global variables
+// this is the variable that takes user input???
+var mapSearchInput = document.querySelector("#map-search-input");
+// do we need a variable for the lat and lng?
+var cordinatesLatLng
+
+// fetch function, when response from API comes back, run refuge API function
+function getMap(location) {
+    // this gets the location of the map - need to have a string for the url
+    var requestURL = "https://" + appID + "&appKey" + "&q=" +
+        fetch(requestURL)
+            .then(function (response) {
+                console.log(response)
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data, location);
+                locationResult(data, location);
+            })
+}
+
+// prevent default behavior of a form 
+function formSubmit(event) {
+    event.preventDefault();
+    var location = cordinatesLatLng.value.trim();
+    if (location) {
+        getMap(location);
+        previoius.unshift({ location });
+        cordinatesLatLng.value = "";
+    }
+    else {
+        swal({
+            title: "Do you need a restroom?",
+            text: "Please enter a location",
+            icon: "warning",
+            buton: "Try Again",
+
+        });
+    }
+    save();
+    history(location);
+}
+
+
+// event listner for formSubmit function
+mapSearchInput.addEventListner("submit", formSubmit);
+
+// global variables for the cordinateLatLngResults function
+var locationResult = document.querySelector("#location");
+var locationAppend = document.querySelector("#location-append");
+
+function locationResult(locationInput) {
+    
+    // clear old data
+    locationAppend.textContent = ""
+    locationResult.textContent = locationInput.q;
+
+    // for loop allows function to run and display more than one result
+    var cards = locationInput.hits;
+    for (var i = 0; i < cards.lenght - 4; i++){
+        var location = cards[i];
+
+        console.log(location);
+
+        // todo we need to take the results and get them on the page... this happens here?!
+        // // creating element to put results in
+        // var locationResultEl = document.createElement("div");
+        // locationResultEl.classList = "card bg-body text"
+
+
+        locationAppend.appendChild(locationResultsEl);
+
+    }
+}
+
+// empty array for searches
+var previous = [];
+// pointer to html
+var pastSearch = document.querySelector("#past-search-results");
+
+
+// function to set local storage
+function save() {
+    localStorage.setItem("previous", JSON.stringify(previoius));
+};
+
+
+// todo local storage thing?
+// create a button element that shows up as a clickable object in order to show its data when clicked
+function history(previous) {
+    oldLocation
+}
+
+    
+
+
+    
+    
+    
+    
+    
+    
+    let map;
 var bathroomsList = [];
 // function initMap() {
 //   map = new google.maps.Map(document.getElementById("map"), {
